765b1ce92688985451513d4d62b1027fa51b5859
sonana2473 <99346498+sonana2473@users.noreply.github.com>
2022-05-12 21:22:19 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,15 +32,15 @@ function preload() {
     imgMercury = loadImage(
       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Solarsystemscope_texture_2k_mercury.jpg/640px-Solarsystemscope_texture_2k_mercury.jpg"
     );
-    imgJupiter = loadImage(
-      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/58d3c051-62a0-4471-818b-b9b92759d6b6/db6is8m-065c1dbf-d125-4729-a015-f79553c16bf9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU4ZDNjMDUxLTYyYTAtNDQ3MS04MThiLWI5YjkyNzU5ZDZiNlwvZGI2aXM4bS0wNjVjMWRiZi1kMTI1LTQ3MjktYTAxNS1mNzk1NTNjMTZiZjkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SNbFKk_oSqXR_g0K2OquQKU33PDgktgkNc40-6Pjhqo"
-    );
-    imgUranus = loadImage(
-      "https://static.vecteezy.com/system/resources/thumbnails/001/998/851/small/abstract-background-of-uranus-surface-free-vector.jpg"
-    );
-    imgNeptune = loadImage(
-      "https://upload.wikimedia.org/wikipedia/commons/1/1e/Solarsystemscope_texture_2k_neptune.jpg"
-    );
+//     imgJupiter = loadImage(
+//       "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/58d3c051-62a0-4471-818b-b9b92759d6b6/db6is8m-065c1dbf-d125-4729-a015-f79553c16bf9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU4ZDNjMDUxLTYyYTAtNDQ3MS04MThiLWI5YjkyNzU5ZDZiNlwvZGI2aXM4bS0wNjVjMWRiZi1kMTI1LTQ3MjktYTAxNS1mNzk1NTNjMTZiZjkuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.SNbFKk_oSqXR_g0K2OquQKU33PDgktgkNc40-6Pjhqo"
+//     );
+//     imgUranus = loadImage(
+//       "https://static.vecteezy.com/system/resources/thumbnails/001/998/851/small/abstract-background-of-uranus-surface-free-vector.jpg"
+//     );
+//     imgNeptune = loadImage(
+//       "https://upload.wikimedia.org/wikipedia/commons/1/1e/Solarsystemscope_texture_2k_neptune.jpg"
+//     );
   }
   myFont = loadFont("assets/myFonts.ttf");
 }
@@ -57,9 +57,9 @@ function setup() {
   sel.option("Earth");
   sel.option("Venus");
   sel.option("Mercury");
-  sel.option("Jupiter");
-  sel.option("Uranus");
-  sel.option("Neptune");
+//   sel.option("Jupiter");
+//   sel.option("Uranus");
+//   sel.option("Neptune");
   sel.changed(mySelectEvent);
 }
 
@@ -146,54 +146,54 @@ function mySelectEvent() {
     c4 = "#595959";
     c5 = "#868686";
   }
-  if (item === "Jupiter") {
-    for (let i = 0; i < numberOfImages; i++) {
-      imgFinal = imgJupiter;
-    }
-    cA1 = color("#735E45");
-    cA2 = color("#D98841");
-    cA3 = color("#D9B29C");
-    cA4 = color("#A64C44");
-    cA5 = color("#C7C4B1");
+//   if (item === "Jupiter") {
+//     for (let i = 0; i < numberOfImages; i++) {
+//       imgFinal = imgJupiter;
+//     }
+//     cA1 = color("#735E45");
+//     cA2 = color("#D98841");
+//     cA3 = color("#D9B29C");
+//     cA4 = color("#A64C44");
+//     cA5 = color("#C7C4B1");
 
-    c1 = "#735E45";
-    c2 = "#D98841";
-    c3 = "#D9B29C";
-    c4 = "#A64C44";
-    c5 = "#C7C4B1";
-  }
-  if (item === "Uranus") {
-    for (let i = 0; i < numberOfImages; i++) {
-      imgFinal = imgUranus;
-    }
-    cA1 = color("#537073");
-    cA2 = color("#95BBBF");
-    cA3 = color("#ADD4D9");
-    cA4 = color("#CEF2EF");
-    cA5 = color("#B5DBDE");
+//     c1 = "#735E45";
+//     c2 = "#D98841";
+//     c3 = "#D9B29C";
+//     c4 = "#A64C44";
+//     c5 = "#C7C4B1";
+//   }
+//   if (item === "Uranus") {
+//     for (let i = 0; i < numberOfImages; i++) {
+//       imgFinal = imgUranus;
+//     }
+//     cA1 = color("#537073");
+//     cA2 = color("#95BBBF");
+//     cA3 = color("#ADD4D9");
+//     cA4 = color("#CEF2EF");
+//     cA5 = color("#B5DBDE");
 
-    c1 = "#537073";
-    c2 = "#95BBBF";
-    c3 = "#ADD4D9";
-    c4 = "#CEF2EF";
-    c5 = "#B5DBDE";
-  }
-  if (item === "Neptune") {
-    for (let i = 0; i < numberOfImages; i++) {
-      imgFinal = imgNeptune;
-    }
-    cA1 = color("#3345A6");
-    cA2 = color("#222A59");
-    cA3 = color("#446FF2");
-    cA4 = color("#4981F2");
-    cA5 = color("#3241A3");
+//     c1 = "#537073";
+//     c2 = "#95BBBF";
+//     c3 = "#ADD4D9";
+//     c4 = "#CEF2EF";
+//     c5 = "#B5DBDE";
+//   }
+//   if (item === "Neptune") {
+//     for (let i = 0; i < numberOfImages; i++) {
+//       imgFinal = imgNeptune;
+//     }
+//     cA1 = color("#3345A6");
+//     cA2 = color("#222A59");
+//     cA3 = color("#446FF2");
+//     cA4 = color("#4981F2");
+//     cA5 = color("#3241A3");
 
-    c1 = "#3345A6";
-    c2 = "#222A59";
-    c3 = "#446FF2";
-    c4 = "#4981F2";
-    c5 = "#3241A3";
-  }
+//     c1 = "#3345A6";
+//     c2 = "#222A59";
+//     c3 = "#446FF2";
+//     c4 = "#4981F2";
+//     c5 = "#3241A3";
+//   }
 }
 
 function draw() {
@@ -379,105 +379,105 @@ function draw() {
     text(c5, width / 2 - 120, 270);
   }
 
-  if (c1 === "#735E45") {
-    cA1 = color("#735E45");
-    cA2 = color("#D98841");
-    cA3 = color("#D9B29C");
-    cA4 = color("#A64C44");
-    cA5 = color("#C7C4B1");
+//   if (c1 === "#735E45") {
+//     cA1 = color("#735E45");
+//     cA2 = color("#D98841");
+//     cA3 = color("#D9B29C");
+//     cA4 = color("#A64C44");
+//     cA5 = color("#C7C4B1");
 
-    text("hehhe", 1000, 500);
-    fill(cA1);
-    rect(width / 2 - 60, 0, 50, 50);
-    fill(200);
-    text(c1, width / 2 - 120, 30);
+//     text("hehhe", 1000, 500);
+//     fill(cA1);
+//     rect(width / 2 - 60, 0, 50, 50);
+//     fill(200);
+//     text(c1, width / 2 - 120, 30);
 
-    fill(cA2);
-    rect(width / 2 - 60, 60, 50, 50);
-    fill(200);
-    text(c2, width / 2 - 120, 90);
+//     fill(cA2);
+//     rect(width / 2 - 60, 60, 50, 50);
+//     fill(200);
+//     text(c2, width / 2 - 120, 90);
 
-    fill(cA3);
-    rect(width / 2 - 60, 120, 50, 50);
-    fill(200);
-    text(c3, width / 2 - 120, 150);
+//     fill(cA3);
+//     rect(width / 2 - 60, 120, 50, 50);
+//     fill(200);
+//     text(c3, width / 2 - 120, 150);
 
-    fill(cA4);
-    rect(width / 2 - 60, 180, 50, 50);
-    fill(200);
-    text(c4, width / 2 - 120, 210);
+//     fill(cA4);
+//     rect(width / 2 - 60, 180, 50, 50);
+//     fill(200);
+//     text(c4, width / 2 - 120, 210);
 
-    fill(cA5);
-    rect(width / 2 - 60, 240, 50, 50);
-    fill(200);
-    text(c5, width / 2 - 120, 270);
-  }
+//     fill(cA5);
+//     rect(width / 2 - 60, 240, 50, 50);
+//     fill(200);
+//     text(c5, width / 2 - 120, 270);
+//   }
 
-  if (c1 === "#537073") {
-    cA1 = color("#537073");
-    cA2 = color("#95BBBF");
-    cA3 = color("#ADD4D9");
-    cA4 = color("#CEF2EF");
-    cA5 = color("#B5DBDE");
+//   if (c1 === "#537073") {
+//     cA1 = color("#537073");
+//     cA2 = color("#95BBBF");
+//     cA3 = color("#ADD4D9");
+//     cA4 = color("#CEF2EF");
+//     cA5 = color("#B5DBDE");
 
-    text("hehhe", 1000, 500);
-    fill(cA1);
-    rect(width / 2 - 60, 0, 50, 50);
-    fill(200);
-    text(c1, width / 2 - 120, 30);
+//     text("hehhe", 1000, 500);
+//     fill(cA1);
+//     rect(width / 2 - 60, 0, 50, 50);
+//     fill(200);
+//     text(c1, width / 2 - 120, 30);
 
-    fill(cA2);
-    rect(width / 2 - 60, 60, 50, 50);
-    fill(200);
-    text(c2, width / 2 - 120, 90);
+//     fill(cA2);
+//     rect(width / 2 - 60, 60, 50, 50);
+//     fill(200);
+//     text(c2, width / 2 - 120, 90);
 
-    fill(cA3);
-    rect(width / 2 - 60, 120, 50, 50);
-    fill(200);
-    text(c3, width / 2 - 120, 150);
+//     fill(cA3);
+//     rect(width / 2 - 60, 120, 50, 50);
+//     fill(200);
+//     text(c3, width / 2 - 120, 150);
 
-    fill(cA4);
-    rect(width / 2 - 60, 180, 50, 50);
-    fill(200);
-    text(c4, width / 2 - 120, 210);
+//     fill(cA4);
+//     rect(width / 2 - 60, 180, 50, 50);
+//     fill(200);
+//     text(c4, width / 2 - 120, 210);
 
-    fill(cA5);
-    rect(width / 2 - 60, 240, 50, 50);
-    fill(200);
-    text(c5, width / 2 - 120, 270);
-  }
+//     fill(cA5);
+//     rect(width / 2 - 60, 240, 50, 50);
+//     fill(200);
+//     text(c5, width / 2 - 120, 270);
+//   }
 
-  if (c1 === "#3345A6") {
-    cA1 = color("#3345A6");
-    cA2 = color("#222A59");
-    cA3 = color("#446FF2");
-    cA4 = color("#4981F2");
-    cA5 = color("#3241A3");
+//   if (c1 === "#3345A6") {
+//     cA1 = color("#3345A6");
+//     cA2 = color("#222A59");
+//     cA3 = color("#446FF2");
+//     cA4 = color("#4981F2");
+//     cA5 = color("#3241A3");
 
-    text("hehhe", 1000, 500);
-    fill(cA1);
-    rect(width / 2 - 60, 0, 50, 50);
-    fill(200);
-    text(c1, width / 2 - 120, 30);
+//     text("hehhe", 1000, 500);
+//     fill(cA1);
+//     rect(width / 2 - 60, 0, 50, 50);
+//     fill(200);
+//     text(c1, width / 2 - 120, 30);
 
-    fill(cA2);
-    rect(width / 2 - 60, 60, 50, 50);
-    fill(200);
-    text(c2, width / 2 - 120, 90);
+//     fill(cA2);
+//     rect(width / 2 - 60, 60, 50, 50);
+//     fill(200);
+//     text(c2, width / 2 - 120, 90);
 
-    fill(cA3);
-    rect(width / 2 - 60, 120, 50, 50);
-    fill(200);
-    text(c3, width / 2 - 120, 150);
+//     fill(cA3);
+//     rect(width / 2 - 60, 120, 50, 50);
+//     fill(200);
+//     text(c3, width / 2 - 120, 150);
 
-    fill(cA4);
-    rect(width / 2 - 60, 180, 50, 50);
-    fill(200);
-    text(c4, width / 2 - 120, 210);
+//     fill(cA4);
+//     rect(width / 2 - 60, 180, 50, 50);
+//     fill(200);
+//     text(c4, width / 2 - 120, 210);
 
-    fill(cA5);
-    rect(width / 2 - 60, 240, 50, 50);
-    fill(200);
-    text(c5, width / 2 - 120, 270);
-  }
+//     fill(cA5);
+//     rect(width / 2 - 60, 240, 50, 50);
+//     fill(200);
+//     text(c5, width / 2 - 120, 270);
+//   }
 }
